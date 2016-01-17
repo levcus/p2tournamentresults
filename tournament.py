55e5017de00f04e192033f50b528b65710d5775a
Leonardo Valle Camacho <Leonardo Valle Camacho>
2016-01-17 17:46:36 -0500

    First commit

---
+++ tournament.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# 
+# tournament.py -- implementation of a Swiss-system tournament
+#
+
+import psycopg2, bleach
+
+
+def connect():
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    return psycopg2.connect("dbname=tournament")
+
+
+def deleteMatches():
+    """Remove all the match records from the database."""
+    db = connect()
+    c = db.cursor()
+    c.execute("DELETE FROM matches;")
+    db.commit()
+    db.close()
+
+def deletePlayers():
+    """Remove all the player records from the database."""
+    db = connect()
+    c = db.cursor()
+    c.execute("DELETE FROM players;")
+    db.commit()
+    db.close()
+
+def countPlayers():
+    """Returns the number of players currently registered."""
+    db = connect()
+    c = db.cursor()
+    c.execute("SELECT count(*) FROM players;")
+    row = c.fetchone()
+    db.close()
+    return int(row[0])
+
+def registerPlayer(name):
+    """Adds a player to the tournament database.
+  
+    The database assigns a unique serial id number for the player.  (This
+    should be handled by your SQL database schema, not in your Python code.)
+  
+    Args:
+      name: the player's full name (need not be unique).
+    """
+    db = connect()
+    c = db.cursor()
+    c.execute("INSERT INTO players (name, wins, losses, matches) VALUES        \
+        (%s, 0, 0, 0);", (bleach.clean(name),))
+    db.commit()
+    db.close()
+
+def playerStandings():
+    """Returns a list of the players and their win records, sorted by wins.
+
+    The first entry in the list should be the player in first place, or a player
+    tied for first place if there is currently a tie.
+
+    Returns:
+      A list of tuples, each of which contains (id, name, wins, matches):
+        id: the player's unique id (assigned by the database)
+        name: the player's full name (as registered)
+        wins: the number of matches the player has won
+        matches: the number of matches the player has played
+    """
+    db = connect()
+    c = db.cursor()
+    c.execute("SELECT id, name, wins, matches FROM players ORDER BY wins;")
+    rows = c.fetchall()
+    db.close()
+    return rows
+    
+def reportMatch(winner, loser):
+    """Records the outcome of a single match between two players.
+
+    Args:
+      winner:  the id number of the player who won
+      loser:  the id number of the player who lost
+    """
+    db = connect()
+    c = db.cursor()
+    c.execute("UPDATE players SET wins = wins + 1, matches = matches + 1 WHERE \
+        id = %s;", (bleach.clean(winner),))
+    c.execute("UPDATE players SET losses = losses + 1, matches = matches + 1   \
+        WHERE id = %s;", (bleach.clean(loser),))
+    db.commit()
+    db.close()
+ 
+def swissPairings():
+    """Returns a list of pairs of players for the next round of a match.
+  
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
+  
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+        id1: the first player's unique id
+        name1: the first player's name
+        id2: the second player's unique id
+        name2: the second player's name
+    """
+    db = connect()
+    c = db.cursor()
+    c.execute("SELECT id, name FROM players ORDER BY wins;")
+    rows = c.fetchall()
+    listpairs = []
+    pair = []
+    n = 0
+    for i in range(len(rows)/2):
+        pair.extend(rows[n])
+        n += 1
+        pair.extend(rows[n])
+        listpairs.append(pair)
+        pair = []
+        n += 1
+    db.close()
+    return listpairs
+
+
+
---
+++ tournament.sql
@@ -0,0 +1,30 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+-- Drop tournament database if exists
+DROP DATABASE IF EXISTS tournament;
+
+-- Create tournament database
+CREATE DATABASE tournament;
+
+-- Connect to tournament database in the command line
+\c tournament;
+
+-- Create the players table
+
+CREATE TABLE players (id SERIAL PRIMARY KEY, name TEXT, wins INTEGER,
+    losses INTEGER, matches INTEGER);
+
+-- Create the matches table
+
+CREATE TABLE matches (playerA INTEGER REFERENCES players(id), playerB INTEGER 
+	REFERENCES players(id), winner INTEGER REFERENCES players(id), 
+	loser INTEGER REFERENCES players(id), PRIMARY KEY (playerA, playerB));
+                                    
+
+
